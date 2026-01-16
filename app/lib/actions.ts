b38903ebcfa62b4da393327b60647a68b632a060
tanghaojin <1226801602@qq.com>
2026-01-16 19:10:05 +0800

    modified:   app/lib/actions.ts 	modified:   app/ui/invoices/create-form.tsx 	modified:   package.json

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -69,36 +69,20 @@ export async function createInvoice(prevState: State, formData: FormData) {
 
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function updateInvoice(
-  id: string,
-  prevState: State,
-  formData: FormData,
-) {
-  const validatedFields = UpdateInvoice.safeParse({
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
-  if (!validatedFields.success) {
-    return {
-      errors: validatedFields.error.flatten().fieldErrors,
-      message: 'Missing Fields. Failed to Update Invoice.',
-    };
-  }
-
-  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
 
-  try {
-    await sql`
+  await sql`
       UPDATE invoices
       SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
       WHERE id = ${id}
     `;
-  } catch (error) {
-    return { message: 'Database Error: Failed to Update Invoice.' };
-  }
 
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
